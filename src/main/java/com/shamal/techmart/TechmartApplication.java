8c598a7aec0185eb6f5572f09d7a08322e364904
uwin abeywickrama <uwin.ma@gmail.com>
2022-03-01 08:24:50 +0530

    first commit

---
+++ src/main/java/com/shamal/techmart/TechmartApplication.java
@@ -0,0 +1,13 @@
+package com.shamal.techmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shamal/techmart/TechmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shamal.techmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
